f7ba9552f9ccbe3821a017f825d405a3c5a18fb2
Certil Remy <certil@noukod.com>
2018-10-01 17:20:28 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -71,9 +71,9 @@ def adding_matz
 	programmer_hash =
 
  		{
-			yukihiro_matsumoto => {
+			:yukihiro_matsumoto => {
 			  :known_for => "Ruby",
-			 :languages => ["LISP", "C"]
+			  :languages => ["LISP", "C"]
 			 },
 
         :grace_hopper => {
